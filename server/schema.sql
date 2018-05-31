c58d2af2bec0c5e86ee66e7902384af9367792c1
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-05-31 08:19:22 -0700

    Updated schema.

--- server/schema.sql
+++ server/schema.sql
@@ -1,35 +1,47 @@
-DROP DATABASE IF EXISTS airfecReservations;
-CREATE DATABASE airfecReservations;
+DROP DATABASE IF EXISTS airFeCReservations;
+CREATE DATABASE airFeCReservations;
 
-USE airfecReservations;
+USE airFeCReservations;
 
-CREATE TABLE reservations (
+CREATE TABLE listings (
   roomId INT NOT NULL PRIMARY KEY,
   ratingsScore DECIMAL(2,1),
   ratingsCount SMALLINT,
   maxGuests TINYINT,
-  minNightStay SMALLINT,
-  adultsReserved TINYINT,
-  childsReserved TINYINT,
-  infantsReserved TINYINT
+  minNightStay SMALLINT
 );
 
-CREATE TABLE datesAndPrices (
+CREATE TABLE availabilities (
   roomId INT NOT NULL,
   date date,
   pricePerNight SMALLINT,
   cleaningFee SMALLINT,
   serviceFee SMALLINT,
   FOREIGN KEY (roomId)
-    REFERENCES reservations(roomId)
+    REFERENCES listings(roomId)
+);
+
+CREATE TABLE bookings (
+    bookingId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    roomId INT NOT NULL,
+    checkInDate date,
+    checkOutDate data,
+    roomPrice SMALLINT,
+    cleaningFees SMALLINT,
+    serviceFees SMALLINT,
+    adultsReserved TINYINT,
+    childsReserved TINYINT,
+    infantsReserved TINYINT,
+    FOREIGN KEY (roomId)
+      REFERENCES listings(roomId)
 );
 
 
-INSERT INTO reservations (roomId, ratingsScore, ratingsCount, maxGuests, minNightStay) VALUES (123456, 4.5, 460, 4, 3);
-INSERT INTO datesAndPrices (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-05', 215, 23, 23);
-INSERT INTO datesAndPrices (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-06', 215, 23, 23);
-INSERT INTO datesAndPrices (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-07', 245, 23, 23);
-INSERT INTO datesAndPrices (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-12', 245, 23, 23);
-INSERT INTO datesAndPrices (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-13', 245, 23, 23);
-INSERT INTO datesAndPrices (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-14', 195, 23, 23);
-INSERT INTO datesAndPrices (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-15', 245, 23, 23);
+INSERT INTO listings (roomId, ratingsScore, ratingsCount, maxGuests, minNightStay) VALUES (123456, 4.5, 460, 4, 3);
+INSERT INTO availabilities (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-05', 215, 23, 23);
+INSERT INTO availabilities (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-06', 215, 23, 23);
+INSERT INTO availabilities (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-07', 245, 23, 23);
+INSERT INTO availabilities (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-12', 245, 23, 23);
+INSERT INTO availabilities (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-13', 245, 23, 23);
+INSERT INTO availabilities (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-14', 195, 23, 23);
+INSERT INTO availabilities (roomId, date, pricePerNight, cleaningFee, serviceFee) VALUES (123456, '2018-12-15', 245, 23, 23);
