cc59f490ca44988c5027c94a07aad713e550e266
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-01 19:57:05 -0700

    Setup server files with skeleton express setup.

--- server/server.js
+++ server/server.js
@@ -10,3 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../public'));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
