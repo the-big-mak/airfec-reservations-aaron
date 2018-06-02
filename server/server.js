f01b9a4494489e83fe30fcc6f70c750e30479eb9
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-01 19:54:57 -0700

    Setup server files with skeleton express setup.

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const controller = require('./controller.js');
+const bodyParser = require('body-parser');
+const express = require ('express');
+
+
+const app = express();
+const PORT = process.ENV.route || 3004;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/../public'));
