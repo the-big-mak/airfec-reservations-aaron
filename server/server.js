590f0b474853178e702200d7115f6a12bc9a12e6
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-04 15:00:28 -0700

    Refactor server file to use controller helper file for get and post requests.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 const controller = require('./controller.js');
 const bodyParser = require('body-parser');
 const express = require ('express');
-
+const controller = require('./controller');
 
 const app = express();
 const PORT = process.ENV.route || 3004;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(express.static(__dirname + '/../public'));
-
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+app.use(bodyParser.json())
+  .use(bodyParser.urlencoded({extended: true}))
+  .use(express.static(__dirname + '/../public'))
+  .get('/rooms/:id', controller.get.availNights)
+  .post('/rooms/:id', controller.post.booking)
+  .listen(PORT, () => console.log(`listening on port ${PORT}`));
