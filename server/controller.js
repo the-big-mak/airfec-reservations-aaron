d945ca71ee23e2a5d99ea45f40f4ff62148a62a2
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-04 14:59:52 -0700

    Create controller file to store router helper functions.

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,20 @@
-module.exports = {
+const db = require('./../database/index');
 
+module.exports = {
+  get: {
+    availNights = (req, res) => {
+      db.getAvailNights(res.params.id)
+        .then(data => res.json(data.data))
+        .catch(err => res.sendStatus(500));
+    }
+  },
+  post: {
+    booking = (req, res) => {
+      let booking = res.body;
+      booking.room_id = res.params.id;
+      db.insertBooking(booking)
+        .then(() => res.sendStatus(201))
+        .catch(err => res.status(400).send(err));
+    }
+  }
 };
