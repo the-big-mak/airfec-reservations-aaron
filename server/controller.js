3205898aea556a49d36e416f64a86391c75f322c
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-04 15:03:53 -0700

    Fix spelling errors.

--- server/controller.js
+++ server/controller.js
@@ -3,15 +3,15 @@ const db = require('./../database/index');
 module.exports = {
   get: {
     availNights = (req, res) => {
-      db.getAvailNights(res.params.id)
+      db.getAvailNights(req.params.id)
         .then(data => res.json(data.data))
         .catch(err => res.sendStatus(500));
     }
   },
   post: {
     booking = (req, res) => {
-      let booking = res.body;
-      booking.room_id = res.params.id;
+      let booking = req.body;
+      booking.room_id = req.params.id;
       db.insertBooking(booking)
         .then(() => res.sendStatus(201))
         .catch(err => res.status(400).send(err));
