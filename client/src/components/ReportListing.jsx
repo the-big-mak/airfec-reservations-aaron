a2604ac986de5411789074953f371a0a9fd3a5c9
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-05 16:04:05 -0700

    Add new ReportListing component skeleton.

---
+++ client/src/components/ReportListing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ReportListing = () => (
+  <DivContainer />
+);
+
+const DivContainer = styled.div`
+
+`;
+
+export default ReportListing;
