963471716fc540291048ecb8bf5b65bd717148ac
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-05 14:11:12 -0700

    Refactor get route to send room details as well.

--- server/controller.js
+++ server/controller.js
@@ -2,17 +2,25 @@ const db = require('./../database/index');
 
 module.exports = {
   get: {
-    availNights: function (req, res) {
-      return db.getAvailNights(req.params.id)
-        .then(data => res.json(data))
-        .catch(err => res.status(500).send(err));
+    roomDetailsAndAvailNights: function (req, res) {
+      let data = [];
+      db.getRoomDetails(req.params.id)
+      .then(roomDetails => {
+        data.push(roomDetails);
+        return db.getAvailNights(req.params.id);
+      })
+      .then(availNights => {
+        data.push(availNights);
+        res.json(data);
+      })
+      .catch(err => res.status(500).send(err));
     }
   },
   post: {
     booking: function (req, res) {
       let booking = req.body;
       booking.room_id = req.params.id;
-      return db.insertBooking(booking)
+      db.insertBooking(booking)
         .then(() => res.sendStatus(201))
         .catch(err => res.status(400).send(err));
     }
