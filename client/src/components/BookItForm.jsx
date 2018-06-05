de3ee5192e3a6a7c9c514e49358307ee2914a21d
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-05 15:05:23 -0700

    Stub out calendar component.

---
+++ client/src/components/BookItForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BookItForm = () => (
+  <DivContainer>
+    Calendar
+  </DivContainer>
+);
+
+const DivContainer = styled.div`
+
+`;
+
+export default BookItForm;
