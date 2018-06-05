a519be3717962e7f6eb6e3721c0510512b41b615
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-05 16:00:24 -0700

    Add skeleton styling to Booking Form.

--- client/src/components/BookItForm.jsx
+++ client/src/components/BookItForm.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import BookingButton from './BookingButton'
 
-const BookItForm = () => (
-  <DivContainer>
-    Calendar
-  </DivContainer>
+const BookItForm = ({ availNights, maxGuests, minNightStay, cleaningFee, addtlGuestFee, postBooking }) => (
+  <FormContainer>
+    <div>
+      Calendar Will Go Here
+    </div>
+    <BookingButton />
+    <DivNotChargedYet>
+      <SmallInnerNotChargedYet>You won’t be charged yet</SmallInnerNotChargedYet>
+    </DivNotChargedYet>
+  </FormContainer>
 );
 
-const DivContainer = styled.div`
+const FormContainer = styled.form`
 
 `;
 
+const DivNotChargedYet = styled.div`
+  margin-top: 8px;
+  text-align: center;
+`;
+
+const SmallInnerNotChargedYet = styled.small`
+  color: #484848;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif;
+  font-size: 12px;
+  font-weight: 600;
+  letter-spacing: normal;
+  line-height: 16px;
+  margin: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  word-wrap: break-word;
+`;
 export default BookItForm;
