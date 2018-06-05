cf34e53767fa16e7ff7556fd8d7ba3708e11a75c
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-04 19:20:37 -0700

    Load App component with files from server.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Reservation from './Reservations'
+const ROOM = 42;
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      availNights: []
+    }
+  }
+  componentDidMount () {
+    this.fetch(ROOM);
+  }
+  fetch (roomId) {
+    axios.get(`/rooms/${roomId}`)
+      .then(db => this.setState({availNights: db.data}))
+      .catch(console.err);
   }
   render() {
     return (
