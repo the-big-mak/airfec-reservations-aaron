6326620d3611f6ba2c0fc5190a1827f33e089a11
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-05 17:52:10 -0700

    Test changing static roomId for star rating.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Reservations from './Reservations';
-const ROOM = 42;
+const ROOM = 22;
 
 class App extends Component {
   constructor() {
