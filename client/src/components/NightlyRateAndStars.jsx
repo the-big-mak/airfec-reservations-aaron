c7329e2ed2cb9ea2cdb11fcc669cdc02379fb716
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-07 03:01:23 -0700

    Add skeleton NightlyRateAndStars component.

--- client/src/components/NightlyRateAndStars.jsx
+++ client/src/components/NightlyRateAndStars.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import NightlyRate from './NightlyRate';
 import Stars from './Stars';
 
 const NightlyRateAndStars = ({ avgNightlyRate, stars, totRatings }) => (
   <DivOuterContainer>
-    <NightlyRate avgNightlyRate={avgNightlyRate}/>
-    <Stars stars={stars} totRatings={totRatings}/>
+    <NightlyRate avgNightlyRate={avgNightlyRate} />
+    <Stars stars={stars} totRatings={totRatings} />
     <DivBottomSeparator />
   </DivOuterContainer>
 );
+
+NightlyRateAndStars.propTypes = {
+  avgNightlyRate: PropTypes.number.isRequired,
+  stars: PropTypes.number.isRequired,
+  totRatings: PropTypes.number.isRequired,
+};
 
 const DivOuterContainer = styled.div`
 
