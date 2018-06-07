11e4105aa69d00fcdfda809aaa3ab0f4b4257c27
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-07 03:08:04 -0700

    Create virtual path prefix to serve files to, and rename endpoints.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const controller = require('./controller');
 const bodyParser = require('body-parser');
-const express = require ('express');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3004;
 
 app.use(bodyParser.json())
-  .use(bodyParser.urlencoded({extended: true}))
-  .use(express.static(__dirname + '/../public'))
-  .get('/rooms/:id', controller.get.roomDetailsAndAvailNights)
-  .post('/rooms/:id', controller.post.booking)
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use('/rooms/:id', express.static(path.join(__dirname, '/../public')))
+  .get('/reservations/:id', controller.get.roomDetailsAndAvailNights)
+  .post('/reservations/:id', controller.post.booking)
   .listen(PORT, () => console.log(`listening on port ${PORT}`));
