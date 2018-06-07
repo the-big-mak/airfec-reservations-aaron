7a2787bd1fd3a142e0a1f4a6c971e6de405dbf88
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-07 03:08:47 -0700

    Add DatePicker component placeholder.

---
+++ client/src/components/DatePicker.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DatePicker = () => (
+  <DivContainer>
+    DatePicker Will Go Here
+  </DivContainer>
+);
+
+const DivContainer = styled.div`
+  margin-top: 16px;
+  margin-bottom: 8px;
+`;
+
+export default DatePicker;
