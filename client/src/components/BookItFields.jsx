b028b05c91abc742e37c19cd24b123c2b8957cfb
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-07 23:18:16 -0700

    Update component to pass down more props to children.

--- client/src/components/BookItFields.jsx
+++ client/src/components/BookItFields.jsx
@@ -4,21 +4,40 @@ import styled from 'styled-components';
 import DatePicker from './DatePicker';
 import GuestPicker from './GuestPicker';
 
-const BookItFields = ({ guestDropDownActive, handleGuestDropDown, maxGuests }) => (
+const BookItFields = ({
+  availNights, dateDropDownActive, handleDateDropDown,
+  guestDropDownActive, handleGuestDropDown, maxGuests,
+  minNightStay, handleOutsideDropDownClick,
+}) => (
   <DivContainer>
-    <DatePicker />
+    <DatePicker
+      availNights={availNights}
+      dateDropDownActive={dateDropDownActive}
+      handleDateDropDown={handleDateDropDown}
+      minNightStay={minNightStay}
+      handleOutsideDropDownClick={handleOutsideDropDownClick}
+    />
     <GuestPicker
       guestDropDownActive={guestDropDownActive}
       handleGuestDropDown={handleGuestDropDown}
       maxGuests={maxGuests}
+      handleOutsideDropDownClick={handleOutsideDropDownClick}
     />
   </DivContainer>
 );
 
 BookItFields.propTypes = {
+  availNights: PropTypes.arrayOf(PropTypes.object).isRequired,
+  dateDropDownActive: PropTypes.shape({
+    checkIn: PropTypes.bool,
+    checkOut: PropTypes.bool,
+  }).isRequired,
+  handleDateDropDown: PropTypes.func.isRequired,
   guestDropDownActive: PropTypes.bool.isRequired,
   handleGuestDropDown: PropTypes.func.isRequired,
   maxGuests: PropTypes.number.isRequired,
+  minNightStay: PropTypes.number.isRequired,
+  handleOutsideDropDownClick: PropTypes.func.isRequired,
 };
 
 const DivContainer = styled.div`
