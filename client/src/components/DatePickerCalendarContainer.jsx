50f3ed82d8825f6553e8b765f4d42909c8186baf
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-08 04:10:54 -0700

    Implement skeleton DatePickerCalendarContainer component.

--- client/src/components/DatePickerCalendarContainer.jsx
+++ client/src/components/DatePickerCalendarContainer.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import DatePickerCalendarHeaderDayOfWkItem from './DatePickerCalendarHeaderDayOfWkItem';
+import DatePickerCalendarBackForwardButton from './DatePickerCalendarBackForwardButton';
+import DatePickerCalendarBody from './DatePickerCalendarBody';
 
 const DatePickerCalendarContainer = ({ availNights }) => (
   <DivOuterContainer>
-    Placeholder Text
+    <DivInnerContainerHeader>
+      <DivInnerContainerHeaderInner>
+        <ListCalendarDayOfWeekRow>
+          {['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+            .map(el => <DatePickerCalendarHeaderDayOfWkItem day={el} key={el} />)}
+        </ListCalendarDayOfWeekRow>
+      </DivInnerContainerHeaderInner>
+    </DivInnerContainerHeader>
+    <DivInnerContainerBody>
+      <DivButtonsContainer>
+        <DatePickerCalendarBackForwardButton position="left" />
+        <DatePickerCalendarBackForwardButton position="right" />
+      </DivButtonsContainer>
+      <DivCalendarBodyContainer>
+        <DatePickerCalendarBody availNights={availNights} />
+      </DivCalendarBodyContainer>
+    </DivInnerContainerBody>
   </DivOuterContainer>
 );
 
@@ -12,7 +31,47 @@ DatePickerCalendarContainer.propTypes = {
   availNights: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 const DivOuterContainer = styled.div`
+  width: 318px;
+`;
 
+const DivInnerContainerHeader = styled.div`
+  position: relative;
+  margin-left: 9px;
+`;
+
+const DivInnerContainerHeaderInner = styled.div`
+  color: rgb(117, 117, 117);
+  left: 0px;
+  padding: 0px 13px;
+  position: absolute;
+  top: 62px;
+  text-align: left;
+  z-index: 2;
+`;
+
+const ListCalendarDayOfWeekRow = styled.ul`
+  font-size: 14px;
+  margin: 1px 0px;
+  padding-left: 0px;
+  padding-right: 0px;
+  list-style: none;
+`;
+
+const DivInnerContainerBody = styled.div`
+  outline: none;
+`;
+
+const DivButtonsContainer = styled.div`
+  position: relative;
+  z-index: 2;
+`;
+
+const DivCalendarBodyContainer = styled.div`
+  border-radius: 3px;
+  height: 298px;
+  position: relative;
+  overflow: hidden;
+  width: 318px;
 `;
 
 export default DatePickerCalendarContainer;
