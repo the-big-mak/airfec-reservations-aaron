d8bf618bd8716a2320c8ace297c02cbb243de569
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-08 16:26:35 -0700

    Refactor to stateless component and move methods to parent.

--- client/src/components/DatePickerCalendarMonth.jsx
+++ client/src/components/DatePickerCalendarMonth.jsx
@@ -1,84 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import moment from 'moment';
 import DatePickerCalendarRow from './DatePickerCalendarRow';
 
-export default class DatePickerCalendarMonth extends Component {
-  constructor(props) {
-    super(props);
-    this.dateContext = moment();
-    this.today = moment();
-    this.monthsArr = moment.months();
-  }
-  getArrayOfRowWeeksArrays() {
-    const blanks = [];
-    for (let i = 0; i < this.firstDayOfMonth(); i += 1) {
-      blanks.push({ day: null, active: null });
-    }
-    const daysInMonth = [];
-    for (let d = 1; d <= this.daysInMonth(); d += 1) {
-      daysInMonth.push({ day: d, active: true });
-    }
-    const totalSlots = [...blanks, ...daysInMonth];
-    const rows = [];
-    let cells = [];
-
-    totalSlots.forEach((row, i) => {
-      if (i % 7 !== 0) {
-        cells.push(row);
-      } else {
-        const insertRow = cells.slice();
-        rows.push(insertRow);
-        cells = [];
-        cells.push(row);
-      }
-      if (i === totalSlots.length - 1) {
-        const lastRow = cells.slice();
-        rows.push(lastRow);
-      }
-    });
-    return rows;
-  }
-  year() {
-    return this.dateContext.format('Y');
-  }
-  month() {
-    return this.dateContext.format('MMMM');
-  }
-  daysInMonth() {
-    return this.dateContext.daysInMonth();
-  }
-  currentDate() {
-    return this.dateContext.get('date');
-  }
-  currentDay() {
-    return this.dateContext.format('D');
-  }
-  firstDayOfMonth() {
-    const { dateContext } = this;
-    const firstDay = moment(dateContext).startOf('month').format('d');
-    return firstDay;
-  }
-  render() {
-    return (
-      <DivOuterContainer>
-        <DivMonthHeaderContainer>
-          <strong>{moment(this.props.month).format('MMMM YYYY')}</strong>
-        </DivMonthHeaderContainer>
-        <TableCalendarCells>
-          <tbody>
-            {this.getArrayOfRowWeeksArrays().map(wk =>
-              <DatePickerCalendarRow key={Math.random()} week={wk} />) }
-          </tbody>
-        </TableCalendarCells>
-      </DivOuterContainer>
-    );
-  }
-}
+const DatePickerCalendarMonth = ({ monthWeekRows, monthYear }) => (
+  <DivOuterContainer>
+    <DivMonthHeaderContainer>
+      <strong>{monthYear}</strong>
+    </DivMonthHeaderContainer>
+    <TableCalendarCells>
+      <tbody>
+        {monthWeekRows.map(wk =>
+          <DatePickerCalendarRow key={Math.random()} week={wk} />)
+        }
+      </tbody>
+    </TableCalendarCells>
+  </DivOuterContainer>
+);
 
 DatePickerCalendarMonth.propTypes = {
-  month: PropTypes.string.isRequired,
+  monthYear: PropTypes.string.isRequired,
+  monthWeekRows: PropTypes.arrayOf(PropTypes.array).isRequired,
 };
 
 const DivOuterContainer = styled.div`
@@ -103,3 +45,5 @@ const TableCalendarCells = styled.table`
   border-spacing: 0px;
   max-width: 100%;
 `;
+
+export default DatePickerCalendarMonth;
