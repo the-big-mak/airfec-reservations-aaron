f676b514f90c2fd400502a8b1f55648dde187a41
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-09 21:42:59 -0700

    Implement views count div to appear when scrolling to a certain point.

---
+++ client/src/components/ViewsCount.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ViewsCount = ({ isBookItFixed, views }) => (
+  <DivOuterContainer isBookItFixed={isBookItFixed}>
+    <DivEmptyContainer>
+      <DivBorder />
+    </DivEmptyContainer>
+    <DivInnerContainer>
+      <DivImage>
+        <DivTextContainer>
+          <Span>This home is on people’s minds.</Span>
+          <DivText>It’s been viewed {views} times in the past week.</DivText>
+        </DivTextContainer>
+      </DivImage>
+    </DivInnerContainer>
+  </DivOuterContainer>
+);
+
+ViewsCount.propTypes = {
+  isBookItFixed: PropTypes.bool.isRequired,
+  views: PropTypes.number.isRequired,
+};
+
+const DivOuterContainer = styled.div`
+  ${props => props.isBookItFixed ? 'max-height: 300px; opacity: 1' : 'max-height: 0px; opacity: 0;'}
+  overflow: hidden;
+  transition: max-height 0.8s, opacity 0.8s linear 0.2s;
+`;
+
+const DivEmptyContainer = styled.div`
+  margin: 24px 0;
+`;
+
+const DivBorder = styled.div`
+  border-bottom: 1px solid #DBDBDB;
+`;
+
+const DivInnerContainer = styled.div`
+  color: #484848;
+  font: normal 14px Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif;
+  letter-spacing: normal;
+  line-height: 18px;
+  margin: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  word-wrap: break-word;
+`;
+
+const DivImage = styled.div`
+  background: no-repeat right center/48px url(https://a0.muscache.com/airbnb/static/page3/icon-uc-light-bulb-b34f4ddc543809b3144949c9e8cfcc8d.gif);
+  margin-top: 0px;
+  transition: margin-top 0.4s;
+`;
+
+const DivTextContainer = styled.div`
+  min-height: 34px;
+  width: 84%;
+`;
+
+const Span = styled.span`
+  color: rgb(72, 72, 72);
+  display: inline;
+  font-weight: 600;
+`;
+
+const DivText = styled.div`
+  margin-top: 6px;
+`;
+
+export default ViewsCount;
