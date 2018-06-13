bc183c01cc85c1d487eb620138aaff6d37a1b276
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-12 23:26:05 -0700

    Refactor to prepare for proxy server.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const DIST_DIR = path.join(__dirname, '/public');
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: 'bundle.js',
+    filename: 'bundleReservations.js',
     path: DIST_DIR,
   },
   module: {
